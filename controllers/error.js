90db8f4d847e3683fc00a6179f40de50fe88df8c
amoroabd <amoroabd@gmail.com>
2021-05-22 20:06:15 +0000

    path defined in error.js file at the controllers folder

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,6 @@
 exports.get404 = function (req, res) {
-  res.status(404).render("404.ejs", { pageTitle: "Page Not Found" });
+  res.status(404).render("../views/404.ejs", {
+    pageTitle: "Page Not Found",
+    path: "../views/404.ejs",
+  });
 };
