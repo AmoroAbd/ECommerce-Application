d751a34a613efbf1f1390ceed0b5b566e21ad4a3
amoroabd <amoroabd@gmail.com>
2021-05-22 21:12:19 +0000

    Store product details

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -9,7 +9,11 @@ exports.getAddProduct = function (req, res) {
 };
 // Product Controller - "/admin/product" - POST
 exports.postAddProduct = function (req, res) {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, price, description);
   product.save();
   res.redirect("/");
 };
